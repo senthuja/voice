8a094cc2f53eefda26db07140059af07245fda95
unknown <senthunithy@gmail.com>
2019-03-18 19:52:05 +0530

    Initial Commit

---
+++ Tamil.py
@@ -0,0 +1,164 @@
+
+from gtts import gTTS    
+#gtts is imported
+audio = 'chat'
+def talkToMe1(audio, lgg = 'en-au'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishAustralia.mp3')
+    return None
+talkToMe1(audio , lgg ='en-au') 
+
+def talkToMe2(audio, lgg = 'en-ca'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishCanada.mp3')
+    return None
+talkToMe2(audio , lgg ='en-ca')
+
+def talkToMe3(audio, lgg = 'en-us'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishUS.mp3')
+    return None
+talkToMe3(audio , lgg ='en-us') 
+
+def talkToMe4(audio, lgg = 'fr-fr'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' FrenchFrance.mp3')
+    return None
+talkToMe4(audio , lgg ='fr-fr') 
+
+def talkToMe5(audio, lgg = 'en-gh'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishGhana.mp3')
+    return None
+talkToMe5(audio , lgg ='en-gh') 
+
+def talkToMe6(audio, lgg = 'en-in'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishIndia.mp3')
+    return None
+talkToMe6(audio , lgg ='en-in') 
+
+def talkToMe7(audio, lgg = 'en-ie'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishIreland.mp3')
+    return None
+talkToMe7(audio , lgg ='en-ie')
+
+def talkToMe8(audio, lgg = 'zh-tw'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' MandarinTiwan.mp3')
+    return None
+talkToMe8(audio , lgg ='zh-tw') 
+
+def talkToMe9(audio, lgg = 'en-nz'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishNewzealand.mp3')
+    return None
+talkToMe9(audio , lgg ='en-nz') 
+
+def talkToMe10(audio, lgg = 'en-ng'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishNigeria.mp3')
+    return None
+talkToMe10(audio , lgg ='en-ng') 
+
+def talkToMe11(audio, lgg = 'en-ph'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishPhillipines.mp3')
+    return None
+talkToMe11(audio , lgg ='en-ph')
+
+def talkToMe12(audio, lgg = 'pt-pt'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' Porththukkal.mp3')
+    return None
+talkToMe12(audio , lgg ='pt-pt') 
+
+def talkToMe13(audio, lgg = 'pt-br'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' PortugueseBrazil.mp3')
+    return None
+talkToMe13(audio , lgg ='pt-br') 
+
+def talkToMe14(audio, lgg = 'en-za'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishSouthAfrica.mp3')
+    return None
+talkToMe14(audio , lgg ='en-za') 
+
+def talkToMe15(audio, lgg = 'es-us'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' SpanishUS.mp3')
+    return None
+talkToMe15(audio , lgg ='es-us') 
+
+def talkToMe16(audio, lgg = 'ta'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' Tamil.mp3')
+    return None
+talkToMe16(audio , lgg ='ta')
+
+def talkToMe17(audio, lgg = 'en-tz'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishTanzania.mp3')
+    return None
+talkToMe17(audio , lgg ='en-tz')
+
+def talkToMe18(audio, lgg = 'zh-cn'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' MandarinChina.mp3')
+    return None
+talkToMe18(audio , lgg ='zh-cn') 
+
+def talkToMe19(audio, lgg = 'en-uk'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishUK.mp3')
+    return None
+talkToMe19(audio , lgg ='en-uk') 
+
+def talkToMe20(audio, lgg = 'en-gb'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishUSgb.mp3')
+    return None
+talkToMe20(audio , lgg ='en-gb') 
+
+def talkToMe21(audio, lgg = 'en-us'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' EnglishUS.mp3')
+    return None
+talkToMe21(audio , lgg ='en-us')
+
+def talkToMe22(audio, lgg = 'hi'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' Hindi.mp3')
+    return None
+talkToMe22(audio , lgg ='hi') 
+
+def talkToMe23(audio, lgg = 'fr-ca'):
+    #print(audio)
+    tts = gTTS(text = audio, lang = lgg)
+    tts.save(' FrenchCanada.mp3')
+    return None
+talkToMe23(audio , lgg ='fr-ca') 
